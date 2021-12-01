fd2d78c1465bb219f704d3e11a5729039201e637
danieljmann96 <djmann1996@gmail.com>
2021-12-01 11:52:34 +0000

    day 1 part 2

--- day1.js
+++ day1.js
@@ -12,5 +12,16 @@ read('day1input.txt', 'utf8', function (err, buffer) {
       }
     }
     console.log(`Part 1 answer: ${count}`);
+    const sums = [];
+    for (let i = 0; i < numArr.length - 2; i++) {
+      sums.push(numArr[i] + numArr[i + 1] + numArr[i + 2]);
+    }
+    let count2 = 0;
+    for (let i = 1; i < sums.length; i++) {
+      if (sums[i] > sums[i - 1]) {
+        count2++;
+      }
+    }
+    console.log(`Part 2 answer: ${count2}`);
   }
 });

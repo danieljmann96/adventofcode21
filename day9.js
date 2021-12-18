10c6fe792bb05bfed0541ae67086c0439b68d92c
danieljmann96 <djmann1996@gmail.com>
2021-12-18 12:18:30 +0000

    day 9 part 2

--- day9.js
+++ day9.js
@@ -21,29 +21,51 @@ read('day9input.txt', 'utf8', function (err, buffer) {
       .reverse()
       .map((row, y) => row.split('').map((val, x) => new Point(x, y, Number(val))))
       .flat();
-    const lowValues = [];
-    points.forEach(point => {
-      const adjacentValues = [];
+    function getAdjacentPoints(point) {
+      const adjacentPoints = [];
       const left = points.find(elem => elem.x === point.x - 1 && elem.y === point.y);
       if (left) {
-        adjacentValues.push(left.value);
+        adjacentPoints.push(left);
       }
       const right = points.find(elem => elem.x === point.x + 1 && elem.y === point.y);
       if (right) {
-        adjacentValues.push(right.value);
+        adjacentPoints.push(right);
       }
       const up = points.find(elem => elem.x === point.x && elem.y === point.y + 1);
       if (up) {
-        adjacentValues.push(up.value);
+        adjacentPoints.push(up);
       }
       const down = points.find(elem => elem.x === point.x && elem.y === point.y - 1);
       if (down) {
-        adjacentValues.push(down.value);
+        adjacentPoints.push(down);
       }
-      if (point.value < Math.min(...adjacentValues)) {
-        lowValues.push(point.value);
+      return adjacentPoints;
+    }
+    //PART 1 SOLVED
+    const lowPoints = [];
+    points.forEach(point => {
+      if (point.value < Math.min(...getAdjacentPoints(point).map(x => x.value))) {
+        lowPoints.push(point);
       }
     });
-    console.log(`Part 1 answer: ${lowValues.reduce((a, b) => a + b) + lowValues.length}`);
+    console.log(`Part 1 answer: ${lowPoints.map(x => x.value).reduce((a, b) => a + b) + lowPoints.length}`);
+    //PART 2 UNSOLVED
+    let basins = [];
+    points.forEach(point => {
+      let adjacents = getAdjacentPoints(point).filter(x => x.value > point.value);
+      for (let i = 0; i < adjacents.length; i++) {
+        let adjs = getAdjacentPoints(adjacents[i]).filter(x => x.value > adjacents[i].value);
+        if (adjs.length > 0) {
+          adjacents.push(...adjs);
+        }
+      }
+      basins.push([...new Set(adjacents.filter(x => x.value !== 9).map(x => JSON.stringify(x)))].length + 1);
+    });
+    console.log(
+      `Part 2 answer: ${basins
+        .sort((a, b) => b - a)
+        .slice(0, 3)
+        .reduce((a, b) => a * b)}`
+    );
   }
 });
